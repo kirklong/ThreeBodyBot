810b2059ed56419d42b2b8445370a4e29d35e333
Kirk <kirklong@u.boisestate.edu>
2020-06-14 23:19:19 -0600

    update to 3BodyShell script to reflect new music choices

--- 3BodyShell.sh
+++ 3BodyShell.sh
@@ -21,41 +21,49 @@ rm *.png
 cd /home/kirk/Documents/3Body
 echo 'adding music' >> /home/kirk/Documents/3Body/cron_log.txt
 echo ' ' >> /home/kirk/Documents/3Body/initCond.txt #so next thing goes to new line
-num=$((1+RANDOM%9)) #get number between 1 and 9
+num=$((1+RANDOM%17)) #get number between 1 and 9
 if [ $num -eq 1 ]; then
-  echo 'Music: Adagio for Strings -- Barber' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Adagio for Strings -- Barber' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 2 ]; then
-  echo 'Music: The Blue Danube Waltz -- Strauss' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: The Blue Danube Waltz -- Strauss' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 3 ]; then
-  echo 'Music: Moonlight Sonata (1st Mvmt) -- Beethoven' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Moonlight Sonata (1st Mvmt) -- Beethoven' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 4 ]; then
-  echo 'Music: Clair de Lune -- Debussy' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Clair de Lune -- Debussy' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 5 ]; then
-  echo 'Music: Gymnopedie No. 1 -- Satie' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Gymnopédie No. 1 -- Satie' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 6 ]; then
-  echo 'Music: Symphony No. 5 (1st Mvmt) -- Beethoven' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Symphony No. 5 (1st Mvmt) -- Beethoven' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 7 ]; then
-  echo 'Music: First Step (Interstellar) -- Zimmer' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: First Step (Interstellar) -- Zimmer' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 8 ]; then
-  echo 'Music: Time (Inception) -- Zimmer' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: Time (Inception) -- Zimmer' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 elif [ $num -eq 9 ]; then
-  echo 'Music: I Need a Ride (The Expanse) -- Shorter' >> /home/kirk/Documents/3Body/initCond.txt
+  echo 'Music: I Need a Ride (The Expanse) -- Shorter' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 10 ]; then
+  echo 'Music: Prelude in E Minor -- Chopin' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 11 ]; then
+  echo 'Music: Prelude in C# Minor (Posthumous) -- Chopin' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 12 ]; then
+  echo 'Music: Battlestar Sonatica (BSG) -- McCreary' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 13 ]; then
+  echo 'Music: Rhapsody in Blue (solo piano) -- Gershwin' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 14 ]; then
+  echo 'Music: Passacaglia (BSG, solo piano) -- McCreary' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 15 ]; then
+  echo 'Music: Prelude in G Minor -- Rachmaninoff' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 16 ]; then
+  echo 'Music: Prelude in C# Minor -- Rachmaninoff' >> /home/kirk/Documents/3Body/nbody/initCond.txt
+elif [ $num -eq 17 ]; then
+  echo 'Music: The Shape of Things To Come (BSG, solo piano) -- McCreary' >> /home/kirk/Documents/3Body/nbody/initCond.txt
 fi
-#1=adagio 4 strings
-#2=blue danube
-#3=moonlight
-#4=clair de lune
-#5=gymnopedie 1
-#6=beethoven 5
-#7=first step/Interstellar
-#8=time/Inception
-#9=I need a ride/Expanse
+
 musicFile="/home/kirk/Documents/3Body/music/music_choice_${num}.mp3"
 videoFile="/home/kirk/Documents/3Body/3Body_fps30.mp4"
 combinedFile="3Body_fps30_wMusic.mp4"
 combinedAACOut="3Body_fps30_wMusicAAC.mp4"
 ffmpeg -i $videoFile -i $musicFile -codec copy -shortest $combinedFile #combine audio w/video
-ffmpeg -i $combinedFile -codec:a aac $combinedAACOut #change audio to aac lc format for twitter
+ffmpeg -i $combinedFile -codec:a aac -preset slow $combinedAACOut #change audio to aac lc format for twitter
 cd twitterbot
 ./server.js
 echo 'script ran successfully' >> /home/kirk/Documents/3Body/cron_log.txt
